ffb5a1d9f27163fa7caa4d04e71c6072d2ab9dd8
Rolandroid <117363873+Rolandroid@users.noreply.github.com>
2022-12-22 09:17:59 -0300

    Comienza el Proyecto

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const path = require("path")
+const port = 3000
+
+
+app.use(express.static(path.join(__dirname,".","public")));
+app.listen(port, () => console.log("Port running"))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, ".", "views", "home.html"))
+});
