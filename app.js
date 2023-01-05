3d9247b233a1db1f88c8e8b2237b80638ac28fed
Rolandroid <117363873+Rolandroid@users.noreply.github.com>
2023-01-05 08:10:47 -0300

    arregla app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port = 3000
 
 
 app.use(express.static(path.join(__dirname,".","public")));
-app.listen(port, () => console.log("Port running"))
+app.listen(port, () => console.log("Port " + port + " running"))
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, ".", "views", "header.html"))
