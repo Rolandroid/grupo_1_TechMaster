023d55d0ebb58a047136e9e666da05fb7ff813a0
Rolandroid <117363873+Rolandroid@users.noreply.github.com>
2023-01-06 11:14:40 -0300

    crea estructa mvc

--- app.js
+++ app.js
@@ -7,6 +7,29 @@ const port = 3000
 app.use(express.static(path.join(__dirname,".","public")));
 app.listen(port, () => console.log("Port " + port + " running"))
 
+
+
 app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, ".", "views", "index.html"))
+});
+app.get('/header', (req, res) => {
     res.sendFile(path.resolve(__dirname, ".", "views", "header.html"))
+});
+app.get('/footer', (req, res) => {
+    res.sendFile(path.resolve(__dirname, ".", "views", "footer.html"))
+});
+app.get('/home', (req, res) => {
+    res.sendFile(path.resolve(__dirname, ".", "views", "home.html"))
+});
+app.get('/login', (req, res) => {
+    res.sendFile(path.resolve(__dirname, ".", "views", "login.html"))
+});
+app.get('/register', (req, res) => {
+    res.sendFile(path.resolve(__dirname, ".", "views", "register.html"))
+});
+app.get('/carrito', (req, res) => {
+    res.sendFile(path.resolve(__dirname, ".", "views", "carrito.html"))
+});
+app.get('/detalle', (req, res) => {
+    res.sendFile(path.resolve(__dirname, ".", "views", "detalle.html"))
 });
