580b8987c4c12eb7084f6366f11d20d8b449ab52
BraianAlmiron <braianpatricio1@gmail.com>
2023-02-11 03:57:50 -0300

    agrega edicion de productos

--- controllers/productController.js
+++ controllers/productController.js
@@ -9,6 +9,7 @@ module.exports = {
 
 
     detalle : (req , res) => {
+        const products = JSON.parse(fs.readFileSync(path.join(__dirname, "../data/products.json"), 'utf-8'));
         const {id} = req.params
         const product = products.find(product => product.id === +id)
         console.log(product)
@@ -54,6 +55,38 @@ module.exports = {
 
     },
     edicion : (req , res) => {
-        return res.render('products/edicion')
+        const {id} = req.params;
+        const product = products.find(product => product.id === +id)
+        return res.render('products/edicion',{
+            ...product
+        })
+    },
+
+    update : (req,res) => {
+        const {name, image, description, discount, price, category} = req.body;
+        const id = +req.params.id
+        const product = products.find(product => product.id === +id)
+        const productUpdate = {
+            id,
+            name: name.trim(),
+            price: +price,
+            description: description.trim(),
+            image: product.image,
+            discount: +discount,
+            category: category.trim()
+        };
+
+        const productModified = products.map(product => {
+            if(product.id === id) {
+                return productUpdate
+            }
+
+            return product;
+
+        })
+
+        fs.writeFileSync('./data/products.json',JSON.stringify(productModified, null, 3), 'utf-8')
+        return res.redirect(`/products/detalle/${id}`)
+
     }
 };
--- routes/products.js
+++ routes/products.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-const {detalle,list,creacion,edicion, create} = require('../controllers/productController')
+const {detalle,list,creacion,edicion,create,update} = require('../controllers/productController')
 
 router
 .get('/detalle/:id',detalle)
 .get('/list',list)
 .get('/creacion',creacion)
 .post('/creacion',create)
-.get('/edicion',edicion)
+.get('/edicion/:id',edicion)
+.put('/update/:id',update)
 
 
 
