98f857fcbc1890497245d5e5714b0790fd3bb155
CARLOS-V-G <cv5944341@gmail.com>
2023-02-18 02:05:42 -0300

    Agrega funcion filter shearch

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,3 +1,7 @@
+const fs = require('fs')
+const path = require('path')
+const products = JSON.parse(fs.readFileSync(path.join(__dirname, "../data/products.json"), 'utf-8'));
+
 module.exports = {
     home : (req , res) => {
         return res.render('home')
@@ -10,6 +14,19 @@ module.exports = {
     },
     resultados : (req , res) => {
         return res.render('resultados')
+    },
+
+    search: (req, res) => { 
+        const {keywords} = req.query;
+        if(req.query.keywords.trim() !== ''){
+             let resultSearch = products.filter(product => product.name.toLowerCase().includes(keywords.toLowerCase()) ||  product.description.toLowerCase().includes(keywords.toLowerCase().trim()));
+            res.render('results',{
+                resultSearch,
+                keywords: req.query.keywords,
+            })
+        }else{
+            res.redirect('/')
+        }
     }
    
 }
