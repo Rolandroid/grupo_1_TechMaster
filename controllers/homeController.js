fa51c0d313ec6fbffbbddd7b68bf7019e613d339
Rolandroid <r.lisarazo@gmail.com>
2023-02-24 02:34:50 -0300

    agrega estilos

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -20,8 +20,10 @@ module.exports = {
         const {keywords, category} = req.query;
         if(keywords.trim() !== ''){
             let resultSearch = products.filter(product => 
-                (product.name.toLowerCase().includes(keywords.toLowerCase().trim()) ||  
-                product.description.toLowerCase().includes(keywords.toLowerCase().trim())) && 
+                (product.name.toLowerCase().includes(keywords.toLowerCase().trim()) ||
+                product.category.toLowerCase().includes(keywords.toLowerCase().trim()) ||  
+                product.description.toLowerCase().includes(keywords.toLowerCase().trim()))
+                 && 
                 (category === '' || product.category === category)
             );
             res.render('results',{
