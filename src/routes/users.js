a8eb25f38a652a64d053486ccc859f3a96fb629f
FlorenciaAmado <amadoflorencia8@gmail.com>
2023-03-09 00:18:39 -0300

    agrega middlewares admin & user

--- src/routes/users.js
+++ src/routes/users.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 const {login,register, newPassword, processRegister,processLogin, logout, profile} = require('../controllers/userController');
-const checkUserLogin = require('../middlewares/checkUserLogin');
 const loginUserValidator = require('../validations/loginUserValidator');
 const registerUserValidation = require('../validations/registerUserValidation');
 
@@ -12,7 +11,7 @@ router
 .get('/login',login)
 .post('/login',loginUserValidator,processLogin)
 .get('/newPassword',newPassword)
-.get('/logout',checkUserLogin,logout)
+.get('/logout',logout)
 .get('/profile', profile)
 
 
