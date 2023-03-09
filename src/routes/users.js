544ead34e53f52856daa245687cd8a33f1fa7e05
FlorenciaAmado <amadoflorencia8@gmail.com>
2023-03-09 01:01:08 -0300

    agrega permisos en rutas

--- src/routes/users.js
+++ src/routes/users.js
@@ -2,17 +2,19 @@ const express = require('express');
 const router = express.Router();
 
 const {login,register, newPassword, processRegister,processLogin, logout, profile} = require('../controllers/userController');
+const checkUser = require('../middlewares/checkUser');
+const checkUserLogin = require('../middlewares/checkUserLogin');
 const loginUserValidator = require('../validations/loginUserValidator');
 const registerUserValidation = require('../validations/registerUserValidation');
 
 router
-.get('/register',register)
+.get('/register',checkUser,register)
 .post('/register',registerUserValidation,processRegister)
-.get('/login',login)
+.get('/login',checkUser,login)
 .post('/login',loginUserValidator,processLogin)
 .get('/newPassword',newPassword)
-.get('/logout',logout)
-.get('/profile', profile)
+.get('/logout',checkUserLogin,logout)
+.get('/profile',checkUserLogin, profile)
 
 
 
