f7b304033f2f6b6ca4bfca4f5f3f9ac663095ddb
Rolandroid <r.lisarazo@gmail.com>
2023-04-04 00:40:27 -0300

    crea estructura de base de datos

---
+++ src/database/config/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  "development": {
+    "username": DB_USERNAME,
+    "password": DB_PASSWORD,
+    "database": DB_DATABASE,
+    "host": DB_HOST,
+    "dialect": DB_PORT
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+}
