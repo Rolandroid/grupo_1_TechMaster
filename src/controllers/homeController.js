572d5a800013b23e996cbcffa986d9ff0dc33364
Rolandroid <r.lisarazo@gmail.com>
2023-04-19 08:48:32 -0300

    agrega metodo Search, rompe navBar

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,61 +1,76 @@
 const fs = require('fs')
 const path = require('path')
-const {readJSON, writeJSON} = require('../data')
+const { readJSON, writeJSON } = require('../data')
 const products = JSON.parse(fs.readFileSync(path.join(__dirname, "../data/products.json"), 'utf-8'));
+const db = require("../database/models")
+const { Op } = require("sequelize")
 
 module.exports = {
-    home : (req , res) => {
-        return res.render('home',{
-            products
-        })
-    },
-    carrito : (req , res) => {
-        return res.render('carrito')
-    },
-    detalle : (req , res) => {
-        return res.render('detalle')
-    },
-    resultados : (req , res) => {
-        return res.render('resultados')
-    },
+  home: (req, res) => {
+    return res.render('home', {
+      products
+    })
+  },
+  carrito: (req, res) => {
+    return res.render('carrito')
+  },
+  detalle: (req, res) => {
+    return res.render('detalle')
+  },
+  resultados: (req, res) => {
+    return res.render('resultados')
+  },
 
-    search: (req, res) => { 
-        const {keywords, category} = req.query;
-        if(keywords.trim() !== ''){
-            let resultSearch = products.filter(product => 
-                (product.name.toLowerCase().includes(keywords.toLowerCase().trim()) ||
-                product.category.toLowerCase().includes(keywords.toLowerCase().trim()) ||  
-                product.description.toLowerCase().includes(keywords.toLowerCase().trim()))
-                 && 
-                (category === '' || product.category === category)
-            );
-            
-            res.render('results',{
-                resultSearch,
-                keywords: req.query.keywords,
-            })
-        }else{
-            res.redirect('/')
+  search: async (req, res) => {
+    const { keywords, category } = req.query;
+
+    if (keywords !== '') {
+      try {
+        const resultSearch = await db.Product.findAll({
+          where: {
+            [Op.and]: [{
+              [Op.or]: [{ name: { [Op.substring]: keywords } }, { description: { [Op.substring]: keywords } }]
+            },
+            { categoryId: category },
+            { visible: true }]
+          },
+          include: ['images']
         }
-    },
-    navBar : (req, res) => {
-        let resultSearch = JSON.parse(fs.readFileSync(path.join(__dirname, "../data/products.json"), 'utf-8'));
-        let {category} = req.params        
-        if(category==="discount"){
-         resultSearch = resultSearch.filter(product => product.discount > 1)
-         category = "Oferta"
-        return res.render('results',{resultSearch, keywords: category})
-     }else{resultSearch = resultSearch.filter(product => product.category === category)
-         return res.render('results',{resultSearch, keywords: category})}
-        },
 
+        );
 
-
-    dashboard : (req, res) => {
-        let products = readJSON('products.json')
-        return res.render('dashboard',{products})
-    },
-    aboutUs : (req, res) => {
-      return res.render('aboutUs')
+        res.render('results', {
+          resultSearch,
+          keywords
+        });
+      } catch (error) {
+        console.log(error)
+      }
+    } else {
+      res.redirect('/');
+    }
+  },
+  navBar: (req, res) => {
+    let resultSearch = JSON.parse(fs.readFileSync(path.join(__dirname, "../data/products.json"), 'utf-8'));
+    let { category } = req.params
+    if (category === "discount") {
+      resultSearch = resultSearch.filter(product => product.discount > 1)
+      category = "Oferta"
+      return res.render('results', { resultSearch, keywords: category })
+    } else {
+      resultSearch = resultSearch.filter(product => product.category === category)
+      return res.render('results', { resultSearch, keywords: category })
+    }
+  },
+  dashboard: async (req, res) => {
+    try {
+      const products = await db.Product.findAll({
+        include: ['images']
+      });
+      return res.render('dashboard', { products });
+    } catch (error) { console.log(error) }
+  },
+  aboutUs: (req, res) => {
+    return res.render('aboutUs')
   }
 }
