cf5d823deb01215ddf108e5cd354bc9dfad17911
FlorenciaAmado <amadoflorencia8@gmail.com>
2023-05-21 16:28:21 -0300

    update profile style

--- src/routes/home.js
+++ src/routes/home.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const {home,detalle,carrito,search,navBar,dashboard,aboutUs,comments,contact} = require('../controllers/homeController')
+const {home,carrito,search,navBar,dashboard,aboutUs,comments,contact} = require('../controllers/homeController')
 
 router
 .get('/',home)
 .get('/search',search)
+.get('/carrito',carrito)
 .get('/products/list/:category',navBar)
 .get('/dashboard',dashboard)
 .get('/aboutUs',aboutUs)
