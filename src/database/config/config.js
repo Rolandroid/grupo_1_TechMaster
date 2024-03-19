19eee3120a4dc5f708a3caca9f742d9fbadaabca
rolandroid <r.lisarazo@gmail.com>
2024-03-19 07:11:21 -0300

    crea base de datos interna

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -2,25 +2,15 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database:  process.env.DB_DATABASE,
-    host:  process.env.DB_HOST,
-    dialect: "mysql",
-    port :  process.env.DB_PORT
+    dialect: "sqlite",
+    storage: "src/database/database/database.sqlite"
   },
   test: {
-    username: "root",
-    password: null,
-    database: "database_test",
-    host: "127.0.0.1",
-    dialect: "mysql",
+    dialect: "sqlite",
+    storage: "src/database/database/database.sqlite"
   },
   production: {
-    username: "root",
-    password: null,
-    database: "database_production",
-    host: "127.0.0.1",
-    dialect: "mysql",
-  },
-};
+    dialect: "sqlite",
+    storage: "src/database/database/database.sqlite"
+  }
+}
